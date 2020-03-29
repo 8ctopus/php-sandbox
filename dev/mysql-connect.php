f83e1230cb12b9d140203004529ecbce06892efc
8ctopus <hello@octopuslabs.io>
2020-03-29 10:59:55 +0500

    improved: database connection test

--- dev/mysql-connect.php
+++ dev/mysql-connect.php
@@ -1,13 +1,20 @@
 <?php
 
-$mysqli = new mysqli('dev-db', 'root', '123', 'test');
+/**
+ * Test database connection
+ * based on https://www.php.net/manual/en/mysqli.examples-basic.php
+ */
+
+echo("<pre>");
+
+$mysqli = new mysqli("dev-db", "root", "123", "test");
 
 if ($mysqli->connect_errno)
 {
-    echo('Sorry, this website is experiencing problems.');
-    echo('Error: Failed to make a MySQL connection, here is why: \n');
-    echo('Errno: " . $mysqli->connect_errno . "\n');
-    echo('Error: " . $mysqli->connect_error . "\n');
+    echo("Sorry, this website is experiencing problems.\n");
+    echo("Error: Failed to make a MySQL connection, here is why:\n");
+    echo("Errno.". $mysqli->connect_errno ."\n");
+    echo("Error.". $mysqli->connect_error ."\n");
     exit();
 }
 
@@ -19,35 +26,36 @@ TAG;
 
 if (!$result = $mysqli->query($sql))
 {
-    echo('Sorry, the website is experiencing problems.');
-    echo('Error: Our query failed to execute and here is why: \n');
-    echo('Query: " . $sql . "\n');
-    echo('Errno: " . $mysqli->errno . "\n');
-    echo('Error: " . $mysqli->error . "\n');
+    echo("Sorry, the website is experiencing problems.\n");
+    echo("Error: Our query failed to execute and here is why:\n");
+    echo("Query.". $sql ."\n");
+    echo("Errno.". $mysqli->errno ."\n");
+    echo("Error.". $mysqli->error ."\n");
     exit();
 }
 
-$sql = "SHOW TABLES";
+$sql = <<<TAG
+    SHOW TABLES;
+TAG;
 
 if (!$result = $mysqli->query($sql))
 {
-    echo('Sorry, the website is experiencing problems.');
-    echo('Error: Our query failed to execute and here is why: \n');
-    echo('Query: " . $sql . "\n');
-    echo('Errno: " . $mysqli->errno . "\n');
-    echo('Error: " . $mysqli->error . "\n');
+    echo("Sorry, the website is experiencing problems.\n");
+    echo("Error: Our query failed to execute and here is why:\n");
+    echo("Query.". $sql ."\n");
+    echo("Errno.". $mysqli->errno ."\n");
+    echo("Error.". $mysqli->error ."\n");
     exit();
 }
 
 if ($result->num_rows === 0)
 {
-    echo('Sorry, the website is experiencing problems.');
-    echo('Error: Our query failed to execute and here is why: \n');
-    echo('test database has no tables');
+    echo("Sorry, the website is experiencing problems.\n");
+    echo("Error: Our query failed to execute and here is why:\n");
+    echo("test database has no tables\n");
     exit();
 }
 
-echo('<pre>');
 print_r($result->fetch_assoc());
 
-echo('<h1>It works!<h1>');
+echo("<h1>It works!<h1>\n");
