3dd38ea65a134388a117988b4df461ee60c50aad
8ctopus <hello@octopuslabs.io>
2020-03-30 08:33:35 +0500

    improved: mariadb image now has persistent database

--- mariadb/include/start.sh
+++ mariadb/include/start.sh
@@ -1,8 +1,26 @@
-# start mariadb service
-rc-service mariadb start;
+# check if database exists
+if [ -e /var/lib/mysql/ib_logfile0 ]
+then
+    echo "Database already exists"
 
-# configure database
-mysql < /init.sql;
+    # start mariadb service
+    echo "Start mariadb service..."
+    rc-service mariadb restart
+else
+    # create database
+    echo "Install database..."
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
-# start zsh
-zsh;
+    # start mariadb service
+    echo "Install database - start mariadb service..."
+    rc-service mariadb start
+
+    # configure database
+    echo "Install database - configure users..."
+    mysql < /init.sql
+
+    echo "Install database - OK"
+fi
+
+# wait forever
+tail -f /dev/null
