918af8c02c552c9c442916087eb1cf81c9c7b8d7
8ctopus <hello@octopuslabs.io>
2020-03-30 14:35:01 +0500

    improved: web image now gracefully stops improved: web image console logging

--- web/include/start.sh
+++ web/include/start.sh
@@ -1,3 +1,29 @@
-httpd
+# start apache2
+echo ""
+echo "Start container web server..."
+httpd -k start
 
-zsh
+echo "-----------------------------------------------"
+echo "Start container web server - OK - Apache ready for connections"
+
+stop_container()
+{
+    echo ""
+    echo "Stop container web server... - received termination signal"
+
+    # stop apache2
+    echo "Stop apache2..."
+    httpd -k stop
+
+    echo "Stop container web server - OK"
+    exit
+}
+
+# catch termination signals
+trap stop_container INT SIGINT SIGQUIT SIGTERM SIGABRT
+
+# infinite loop, will only stop on termination signal
+while true; do
+    sleep 5
+    echo -n "."
+done
