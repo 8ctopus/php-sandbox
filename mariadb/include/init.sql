31bf9b37a31547cb37b02223aaa11b6df2423f7d
8ctopus <hello@octopuslabs.io>
2020-03-31 07:35:14 +0500

    fixed: mariadb initialization script grant privileges

--- mariadb/include/init.sql
+++ mariadb/include/init.sql
@@ -2,5 +2,6 @@ USE `mysql`;
 
 # create user root with access from any host
 CREATE USER 'root'@'%' IDENTIFIED BY '123';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 
 FLUSH PRIVILEGES;
