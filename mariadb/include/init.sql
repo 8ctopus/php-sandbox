f5abd4166442bad36efcd7522b0381778b2c8dd2
8ctopus <hello@octopuslabs.io>
2020-04-01 08:53:14 +0500

    moved: docker images to separate repositories

--- mariadb/include/init.sql
+++
@@ -1,7 +0,0 @@
-USE `mysql`;
-
-# create user root with access from any host
-CREATE USER 'root'@'%' IDENTIFIED BY '123';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
-
-FLUSH PRIVILEGES;
