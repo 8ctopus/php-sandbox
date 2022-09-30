d48c01542fa1b198e9f53a6678656882b895ed2d
8ctopus <hello@octopuslabs.io>
2022-09-30 11:33:17 +0500

    improved: database example

--- html/public/database.php
+++ html/public/database.php
@@ -40,39 +40,72 @@ $db = new PDO("mysql:host={$params['host']};dbname={$params['database']};charset
 ]);
 
 $sql = <<<SQL
-    CREATE TABLE IF NOT EXISTS `test` (
-        `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-    )
+    DROP TABLE IF EXISTS test
 SQL;
 
 $query = $db->prepare($sql);
 $query->execute();
 
 $sql = <<<SQL
-    CREATE TABLE IF NOT EXISTS `test2` (
-        `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-    )
+    CREATE TABLE test (
+        `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        `birthday` DATE NOT NULL,
+        `name` VARCHAR(40) NOT NULL,
+        `salary` INT NOT NULL,
+        `boss` BIT NOT NULL
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8
 SQL;
 
 $query = $db->prepare($sql);
 $query->execute();
 
 $sql = <<<SQL
-    SHOW TABLES;
+    INSERT INTO test
+    (birthday, name, salary, boss)
+    VALUES (:birthday, :name, :salary, :boss)
 SQL;
 
 $query = $db->prepare($sql);
-$query->execute();
 
-if ($query->rowCount() === 0) {
-    throw new Exception('database has no tables');
+$staff = [
+    [
+        'birthday' => (new DateTime('1995-05-01'))->format('Y-m-d'),
+        'name' => 'Sharon',
+        'salary' => '200',
+        'boss' => true,
+    ],
+    [
+        'birthday' => (new DateTime('2000-01-01'))->format('Y-m-d'),
+        'name' => 'John',
+        'salary' => '140',
+        'boss' => false,
+    ],
+    [
+        'birthday' => (new DateTime('1985-08-01'))->format('Y-m-d'),
+        'name' => 'Oliver',
+        'salary' => '120',
+        'boss' => false,
+    ],
+];
+
+foreach ($staff as $member) {
+    $query->bindValue('birthday', $member['birthday'], PDO::PARAM_STR);
+    $query->bindValue('name', $member['name'], PDO::PARAM_STR);
+    $query->bindValue('salary', $member['salary'], PDO::PARAM_INT);
+    $query->bindValue('boss', $member['boss'], PDO::PARAM_BOOL);
+    $query->execute();
 }
 
-echo "<p>Database has {$query->rowCount()} tables</p>\n";
-echo "<ul>\n";
+$sql = <<<SQL
+    SELECT *
+    FROM test
+SQL;
+
+$query = $db->prepare($sql);
+$query->execute();
 
 while ($row = $query->fetch()) {
-    echo "<li>{$row['Tables_in_test']}</li>\n";
+    echo "<li>{$row['id']} {$row['birthday']} {$row['name']} {$row['salary']} {$row['boss']}</li>\n";
 }
 
 echo "</ul>\n";
