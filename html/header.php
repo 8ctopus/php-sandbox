5384e0f10b157ea89dd45218a56d5239f88f4960
8ctopus <hello@octopuslabs.io>
2022-09-30 10:04:06 +0500

    improved: use neat css framework

--- html/header.php
+++ html/header.php
@@ -1,7 +1,8 @@
 <html>
 <head>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.css" rel="stylesheet" crossorigin="anonymous">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="css/neat.css" rel="stylesheet">
 </head>
 <body>
-<div class="container mt-5">
-<pre>
+    <a class="home" href="/">Back home</a>
+    <pre>
