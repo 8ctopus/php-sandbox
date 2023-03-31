d2116582c0d27641d6ac80a2ffeda7ebed4e7848
8ctopus <hello@octopuslabs.io>
2023-03-31 10:03:06 +0400

    Improve database example using nette

--- sites/localhost/html/public/database.php
+++ sites/localhost/html/public/database.php
@@ -1,9 +1,5 @@
 <?php
 
-/**
- * Test database connection
- */
-
 declare(strict_types=1);
 
 $autoLoad = '../vendor/autoload.php';
@@ -41,21 +37,9 @@ $params = [
     'pass' => '123',
 ];
 
-$db = new PDO("mysql:host={$params['host']};dbname={$params['database']};charset=utf8", $params['user'], $params['pass'], [
-    // use exceptions
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    // get arrays
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    // better prevention against SQL injections
-    PDO::ATTR_EMULATE_PREPARES => false,
-]);
+$database = new Nette\Database\Connection("mysql:host={$params['host']};dbname={$params['database']};charset=utf8", $params['user'], $params['pass']);
 
-$sql = <<<'SQL'
-    DROP TABLE IF EXISTS test
-SQL;
-
-$query = $db->prepare($sql);
-$query->execute();
+$database->query('DROP TABLE IF EXISTS test');
 
 $sql = <<<'SQL'
     CREATE TABLE test (
@@ -67,16 +51,7 @@ $sql = <<<'SQL'
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8
 SQL;
 
-$query = $db->prepare($sql);
-$query->execute();
-
-$sql = <<<'SQL'
-    INSERT INTO test
-    (birthday, name, salary, boss)
-    VALUES (:birthday, :name, :salary, :boss)
-SQL;
-
-$query = $db->prepare($sql);
+$database->query($sql);
 
 $staff = [
     [
@@ -99,20 +74,24 @@ $staff = [
     ],
 ];
 
-
-foreach ($staff as $member) {
-    bindValues($query, $member)->execute();
+/*
+for ($i = 0; $i < 12; ++$i) {
+    $staff = array_merge($staff, $staff);
 }
 
-$sql = <<<'SQL'
-    SELECT *
-    FROM test
-SQL;
+echo count($staff);
+*/
 
-$query = $db->prepare($sql);
-$query->execute();
 
-while ($row = $query->fetch()) {
+//\spx_profiler_start();
+
+$database->query('INSERT INTO test', $staff);
+
+//\spx_profiler_stop();
+
+$rows = $database->query('SELECT * FROM test');
+
+foreach ($rows as $row) {
     echo "<li>{$row['id']} {$row['birthday']} {$row['name']} {$row['salary']} {$row['boss']}</li>\n";
 }
 
@@ -120,47 +99,3 @@ echo "</ul>\n";
 echo "<h1>Test database - OK!<h1>\n";
 
 require_once '../footer.php';
-
-/**
- * Bind values to PDO statement
- *
- * @param PDOStatement $statement
- * @param array        $data
- *
- * @return PDOStatement
- */
-function bindValues(PDOStatement $statement, array $data) : PDOStatement
-{
-    foreach ($data as $key => $value) {
-        $statement->bindValue($key, $value, typeToParam($value));
-    }
-
-    return $statement;
-}
-
-/**
- * Variable to PDO type
- *
- * @param mixed $value
- *
- * @return int PDO type
- */
-function typeToParam(mixed $value) : int
-{
-    switch ($type = gettype($value)) {
-        case 'boolean':
-            return PDO::PARAM_BOOL;
-
-        case 'integer':
-            return PDO::PARAM_INT;
-
-        case 'NULL':
-            return PDO::PARAM_NULL;
-
-        case 'string':
-            return PDO::PARAM_STR;
-
-        default:
-            throw new Exception("unsupported type - {$type}");
-    }
-}
