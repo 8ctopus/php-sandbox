febbdf580fb678b148105bda169dc72b6f172195
8ctopus <hello@octopuslabs.io>
2025-06-13 15:38:08 +0400

    Improve mailing

--- sites/localhost/html/public/mailer.php
+++ sites/localhost/html/public/mailer.php
@@ -22,15 +22,21 @@ $whoops->pushHandler(new PrettyPageHandler());
 $whoops->register();
 
 $email = 'hello@octopuslabs.io';
+$subject = 'Email subject';
+$body = 'Email text body';
+
+$headers = <<<HEADERS
+From: webmaster@example.com
+Reply-To: webmaster@example.com.
+HEADERS;
+
+if (!mail($email, $subject, $body, $headers)) {
+    throw new Exception('send email');
+}
 
 /*
 $server = 'mail.octopuslabs.io';
 $pass = '';
-
-if (empty($pass)) {
-    throw new Exception('password missing');
-}
-
 $transport = Transport::fromDsn("smtp://{$email}:{$pass}@{$server}:25");
 */
 
@@ -41,12 +47,12 @@ $mailer = new Mailer($transport);
 $email = (new Email())
     ->from($email)
     ->to($email)
-    ->subject('Time for Symfony Mailer!')
-    ->text('Sending emails is fun again!')
+    ->subject($subject)
+    ->text($body)
     ->html('<p>See Twig integration for better HTML integration!</p>');
 
 $mailer->send($email);
 
 ?>
-<p> email sent </p>
+<p> emails sent </p>
 <a href="localhost:8025">check mailpit</a>
